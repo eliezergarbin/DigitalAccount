227ca7fac65193c8f23e7e14b6d274656b3839b0
Eliezer Nascimento <eliezergarbin1@gmail.com>
2023-02-03 22:55:41 -0300

    criando controller e model AddCustomer

---
+++ DigitalAccount/WebApi/Controllers/AddCustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models.AddCustomer;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddCustomerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddCustomer(AddCustomerInput input)
+        {
+            //Chamar use case para adicionar o customer
+            return Created("", input);
+        }
+    }
+}
