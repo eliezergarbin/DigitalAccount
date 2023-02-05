39c897ff0201f3f90b2068cdbb87adcf2f83933d
Eliezer Nascimento <eliezergarbin1@gmail.com>
2023-02-04 23:25:27 -0300

    criando domain

---
+++ DigitalAccount/Domain/Contracts/UseCases/AddCustomer/IAddCustomerUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts.UseCases.AddCustomer
+{
+    public interface IAddCustomerUseCase
+    {
+        void AddCustomer(Entities.Customer customer);
+    }
+}
---
+++ DigitalAccount/Domain/Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Customer
+    {
+
+        public Customer(string name, string email, string document)
+        {
+            Name = name;
+            Email = email;
+            Document = document;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Document { get; private set; }
+    }
+}
